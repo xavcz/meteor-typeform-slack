264e6803ad28fb32acfe3df92e92d075e78c0a9c
xavizalote <xavier.cazalot@gmail.com>
2016-01-25 22:25:07 +0100

    data model + auto form

---
+++ common/collections.js
@@ -0,0 +1,22 @@
+Credentials = new Meteor.Collection('credentials');
+
+
+Credentials.typeformSchema = new SimpleSchema({
+	uid: {type: String},
+	apiKey: {type: String},
+	email: {type: String},
+	firstname: {type: String},
+	lastname: {type: String}
+});
+
+Credentials.slackSchema = new SimpleSchema({
+	team: {type: String},
+	token: {type: String}
+});
+
+Credentials.schema = new SimpleSchema({
+	typeform: {type: Credentials.typeformSchema},
+	slack: {type: Credentials.slackSchema}
+});
+
+Credentials.attachSchema(Credentials.schema);
