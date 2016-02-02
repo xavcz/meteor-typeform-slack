7a197824e399f877e9257162713408e58ed5e7d4
xavizalote <xavier.cazalot@gmail.com>
2016-02-02 19:17:21 +0100

    get rid of autoform, handle errors w/ meteor guide style

--- client/dashboard.js
+++ client/dashboard.js
@@ -1,4 +1,39 @@
-Template.dashboard.onCreated(function () {
-	// will subscribe to rest2ddp pubs
+Template.dashboard.onCreated(function() {
+	this.errors = new ReactiveDict();
 });
 
+Template.dashboard.helpers({
+	errors(fieldName) {
+		return Template.instance().errors.get(fieldName);
+	}
+});
+
+Template.dashboard.events({
+	'submit .connectSlack'(event) {
+		event.preventDefault();
+		const instance = Template.instance();
+
+		const data = {
+			team: event.target.team.value,
+			token: event.target.token.value
+		};
+
+		Credentials.methods.connectSlack.call(data, (err, res) => {
+			debugger
+			if (err) {
+				debugger
+				if (err.error === 'Credentials.methods.connectSlack.not-logged-in') {
+					debugger
+					const errors = {
+						team: [],
+						token: []
+					};
+
+					errors.team.push(err.reason);
+
+					instance.errors.set(errors);
+				}
+			}
+		});
+	}
+});
