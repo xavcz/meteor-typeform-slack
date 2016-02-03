98d6166335ae0aa01b5e531415d5a6f7d09d8a53
xavizalote <xavier.cazalot@gmail.com>
2016-02-03 21:36:44 +0100

    test storing slack credentials in reactive dict

--- client/dashboard.js
+++ client/dashboard.js
@@ -1,5 +1,6 @@
 Template.dashboard.onCreated(function() {
 	this.errors = new ReactiveDict();
+	this.slack = new ReactiveDict();
 });
 
 Template.dashboard.helpers({
@@ -9,30 +10,26 @@ Template.dashboard.helpers({
 });
 
 Template.dashboard.events({
-	'submit .connectSlack'(event) {
+	'submit .connectSlack'(event, instance) {
 		event.preventDefault();
-		const instance = Template.instance();
 
 		const data = {
 			team: event.target.team.value,
 			token: event.target.token.value
 		};
-
+		
 		Credentials.methods.connectSlack.call(data, (err, res) => {
-			debugger
 			if (err) {
-				debugger
-				if (err.error === 'Credentials.methods.connectSlack.not-logged-in') {
-					debugger
-					const errors = {
-						team: [],
-						token: []
-					};
+				// handle slack errors
+				const errors = {
+					team: [],
+					token: []
+				};
 
-					errors.team.push(err.reason);
-
-					instance.errors.set(errors);
-				}
+				errors.team.push(err.reason);
+				instance.errors.set(errors);
+			} else {
+				instance.slack.set(data);
 			}
 		});
 	}
